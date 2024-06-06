6a5842c196f934953f1d86383ada06ce82e32af7
rouzbeh1384 <r.84.asgari@gmail.com>
2024-06-06 23:17:21 +0330

    now should create method to solve a point and create a database  and border paint

--- src/main/java/com/example/demo/controller/PlayGame.java
+++ src/main/java/com/example/demo/controller/PlayGame.java
@@ -2,12 +2,12 @@ package com.example.demo.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Rectangle;
+import org.w3c.dom.Text;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -41,8 +41,14 @@ public class PlayGame implements Initializable {
     @FXML
     private HBox Answer;
 
+    public String word ;
+    public int fall =0;
+    public boolean test=true;
 
-    int fall ;
+    @FXML
+    private Text score;
+
+
     public ArrayList<TextField>Word=new ArrayList<>();
 
     @Override
@@ -51,27 +57,85 @@ public class PlayGame implements Initializable {
         Answer.setSpacing(10);
 
         start.setOnAction(e->{
-            String word =getWord();
+            ClearAll();
+             word =getWord();
+
             for(int i=0;i<word.length();i++){
+                fall=0;
                 TextField a=new TextField();
                 a.setPrefSize(24,25 );
-                Word.add(a);
+                a.setStyle("-fx-background-color:#00fcf8 ");
 
+                Word.add(a);
                 Answer.getChildren().add(a);
             }
+        });
 
+        check.setOnAction(e->{
+            if (Word.size()!=0) {
+                for (int i = 0; i < word.length(); i++) {
 
+                    if (Word.get(i).getText().equals(String.valueOf(word.charAt(i)))) {
+                        Correct(i);
+                    } else if (Word.get(i).getText().equals("")) {
 
+                    } else {
+                        fall++;
+                        Fall();
+
+                    }
+                }
+
+            }
 
         });
 
 
+    }
 
+    private void ClearAll() {
+        int j=Word.size();
+        System.out.println(j);
+        fall=0;
+        Answer.getChildren().clear();
+        Word.clear();
+        visible();
 
     }
 
+    private void Fall() {
+        visible();
+        switch (fall){
+            case 6:six.setVisible(true);
+            case 5:five.setVisible(true);
+            case 4:four.setVisible(true);
+            case 3:three.setVisible(true);
+            case 2:two.setVisible(true);
+            case 1:one.setVisible(true);
+                break;
+
+        }
+    }
+
+    private void visible() {
+        six.setVisible(false);
+        three.setVisible(false);
+        five.setVisible(false);
+        four.setVisible(false);
+        two.setVisible(false);
+        one.setVisible(false);
+    }
+
+    private void Correct(int i ) {
+        Word.get(i).setDisable(true);
+        Word.get(i).setStyle("-fx-background-color: Green");
+
+    }
+
+
     public String getWord(){
-        String word="Java";
+        String word="rouzbeh";
+        // TODO use api and get word
 
 
         return word;
